206245882c39f865e1979f1f806ceb3fd3661551
Amrani Houssain <amranidev@gmail.com>
2018-12-27 21:43:37 +0000

    Update global.js
    Create global computed properties

--- src/mixins/global.js
+++ src/mixins/global.js
@@ -5,7 +5,15 @@ export default {
         }
     },
     computed: {
-
+        currentUser () {
+            return this.$auth.user()
+        },
+        userId () {
+            return this.currentUser.uid
+        },
+        userCollection () {
+            return this.$store.state.firestore.collection('users').doc(this.userId).collection('music')
+        }
     },
     methods: {
 
