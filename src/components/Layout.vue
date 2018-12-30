dd1823691c07eba082c38e3b172bda7eb9516b13
Amrani Houssain <amranidev@gmail.com>
2018-12-30 16:48:18 +0000

    typo

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -88,7 +88,7 @@ export default {
         { title: 'Cloud Firestore', link: 'account_box' },
       ],
       drawer: true,
-      RoutesUtils: RoutesUtils.navigarion
+      RoutesUtils: RoutesUtils.navigation
     }
   },
   methods: {
